7ffe67eb6b0dc3fbb2bbfd59de2da6b0c049bb7d
Prakash Dahal <dahalprakash1720@gmail.com>
2022-02-03 19:36:58 +0545

    Updates

---
+++ precache-manifest.4992cc28c580778dfea72a1637c3b09c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eced623a15a172550d8c85ec39281c6a",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "e6685de096c0d730a5a6",
+    "url": "/react-calculator/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "b42e569ed0b2c70c2011",
+    "url": "/react-calculator/static/css/main.334fccc3.chunk.css"
+  },
+  {
+    "revision": "e6685de096c0d730a5a6",
+    "url": "/react-calculator/static/js/2.4e8f4c9c.chunk.js"
+  },
+  {
+    "revision": "1200db196523e9102c43",
+    "url": "/react-calculator/static/js/3.e0e7633c.chunk.js"
+  },
+  {
+    "revision": "b42e569ed0b2c70c2011",
+    "url": "/react-calculator/static/js/main.74f53920.chunk.js"
+  },
+  {
+    "revision": "67cdbc346034a81178f6",
+    "url": "/react-calculator/static/js/runtime~main.09a5407f.js"
+  }
+]);
